4d7201d5c3ab26fa0d7359e5269e7670ac6bab12
王耀民 <wangyaominde@qq.com>
2018-06-26 11:34:25 +0800

    wangyaominde

---
+++ wechat/auto_resp.py
@@ -0,0 +1,13 @@
+import itchat
+
+
+@itchat.msg_register(itchat.content.TEXT)
+def text_reply(msg):
+    print(msg['User']['NickName'])
+    print(":")
+    print(msg['Text'])
+    print("\n\n")
+    return ("您好，您发的留言我已经收到，我会在看到消息的第一时间回复您，谢谢！(auto response!)")
+
+itchat.auto_login(enableCmdQR=2)
+itchat.run()
