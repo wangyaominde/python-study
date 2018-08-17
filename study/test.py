573e7e83aa50a0593f7ff65ec49e34baa79b8faa
王耀民 <wangyaominde@qq.com>
2018-08-18 02:01:41 +0800

    wangyaominde

--- study/test.py
+++ study/test.py
@@ -1,27 +1,11 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        temp = 0
-        d = {}
-        start = 0
-        for i in range(len(s)):
-            if s[i] in d and start <= d[s[i]] :
-                print("start:",start)
-                start = d[s[i]] +1
-            temp = max(i-start+1,temp)
-            d[s[i]] = i
-            print("d:",d,'\n'"temp:",temp)
-        return temp
-
-a = Solution()
-s = "33123"
-print(a.lengthOfLongestSubstring(s))
-
-"""a="abcabcabc"
+a="abcasigjh"
+d={}
+for i in range(len(a)):
+    d[a[i]] = i
+print(d)
+"""
 if "abc" in a:
     print("yes")
 else :
-    print("no")"""
+    print("no")
+"""
