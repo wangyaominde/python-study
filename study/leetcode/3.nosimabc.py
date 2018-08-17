1cda19c8543d964ff7726b013b8026c52fca7afd
王耀民 <wangyaominde@qq.com>
2018-08-17 18:29:35 +0800

    wangyaominde

--- study/leetcode/3.nosimabc.py
+++ study/leetcode/3.nosimabc.py
@@ -1,37 +1,11 @@
 class Solution:
-    def match(str):
-        a = len(str)
-        while a:
-            a-=1
-            b=a
-            while b>=0:
-                if(str[a]==str[b]):
-                    if (a is not b):
-                        #print(str[b:a])
-                        return str[b:a]
-                b-=1
-
     def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        return(Solution.match(s))
-        '''a = len(s)
-        while a:
-            a-=1
-            b=a
-            while b>=0:
-                if(s[a]==s[b]):
-                    if (a is not b):
-                        print(s[b:a])
-                b-=1
-'''
-
-
-
-
+        slen = len(s)
+        for i in range(slen):
+            for x in range(slen):
+                if s[x]==s[i]:
+                    print(s[x:i])
 
 a = Solution()
-s = "bbbb"
+s = "abcabcbb"
 a.lengthOfLongestSubstring(s)
