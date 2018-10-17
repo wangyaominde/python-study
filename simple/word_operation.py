1ede2fd959974776b79031bbcf8bd35a45237c28
王耀民 <wangyaominde@qq.com>
2018-10-17 09:10:11 +0800

    wangyaominde

--- simple/word_operation.py
+++ simple/word_operation.py
@@ -4,17 +4,19 @@ from docx.shared import RGBColor
 
 Doc = Document()
 
-'''p = Doc.add_paragraph("昆明沃创科技有限公司")
+p = Doc.add_paragraph("昆明沃创科技有限公司")
 p1 = Doc.add_paragraph("培训通知")
 p2 = Doc.add_paragraph("沃创科技〔2017〕 号")
 
 p.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
 p1.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
 p2.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-'''
+
 p = Doc.add_paragraph()
 run = p.add_run("昆明沃创科技有限公司")
 run.font.color.rgb = RGBColor(255,0,0)
+p.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
 
 
 Doc.save('test.docx')
--- wechat/cloudwords.py
+++ wechat/cloudwords.py
@@ -7,14 +7,6 @@ f = open(u'test.txt', 'r').read()
 wordcloud = WordCloud(background_color="white", width=1000,
                       height=860, margin=2).generate(f)
 
-# width,height,margin可以设置图片属性
-
-# generate 可以对全部文本进行自动分词,但是他对中文支持不好,对中文的分词处理请看我的下一篇文章
-# wordcloud = WordCloud(font_path = r'D:\Fonts\simkai.ttf').generate(f)
-# 你可以通过font_path参数来设置字体集
-
-# background_color参数为设置背景颜色,默认颜色为黑色
-
 
 plt.imshow(wordcloud)
 plt.axis("off")
