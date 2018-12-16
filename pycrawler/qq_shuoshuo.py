a0666dafdb0ca03abaa511e169c58c5dd00d72e6
王耀民 <wangyaominde@qq.com>
2018-12-16 19:19:49 +0800

    Update qq_shuoshuo.py

--- pycrawler/qq_shuoshuo.py
+++ pycrawler/qq_shuoshuo.py
@@ -47,7 +47,7 @@ while True:
         divs = selector.xpath('//*[@id="msgList"]/li/div[3]')
 
         #这里使用 a 表示内容可以连续不清空写入
-        with open('qq_word.txt','a',encoding='utf-8') as f:
+        with open('test.txt','a',encoding='utf-8') as f:
             for div in divs:
                 qq_name = div.xpath('./div[2]/a/text()')
                 qq_content = div.xpath('./div[2]/pre/text()')
