fad70fb7a46bb9636ed7737fc82cb1490a32af51
wangyaominde <wangyaominde@qq.com>
2019-01-03 17:31:52 +0800

    use pyserial get name of serial

--- serial/getserialname.py
+++ serial/getserialname.py
@@ -1,15 +1,19 @@
-# @Author: wangyaominde  
-# @Date: 2019-01-03 16:47:57  
-# @Last Modified by:   wangyaominde  
+# @Author: wangyaominde
+# @Date: 2019-01-03 16:47:57
+# @Last Modified by:   wangyaominde
 # @Last Modified time: 2019-01-03 16:47:57
-#%%
+
 import serial.tools.list_ports
 
-
-#%%
-Com_Dict = {}  
-#%%
+restemser = "CH340"
+Com_Dict = {}
 port_list = list(serial.tools.list_ports.comports())
-#%%
 for port in port_list:
             Com_Dict["%s" % port[1]] = "%s" % port[1]
+            if(restemser in port[1]):
+                print(port[0])
+
+port
+port[1]
+port[0]
+port[2]
