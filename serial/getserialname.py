88579a1419b5fc241d5f10f5a346abc898cc711e
wangyaominde <wangyaominde@qq.com>
2019-02-13 09:28:08 +0800

    change

--- serial/getserialname.py
+++ serial/getserialname.py
@@ -2,18 +2,30 @@
 # @Date: 2019-01-03 16:47:57
 # @Last Modified by:   wangyaominde
 # @Last Modified time: 2019-01-03 16:47:57
-
 import serial.tools.list_ports
+import serial
+import string
+import binascii
 
-restemser = "CH340"
-Com_Dict = {}
-port_list = list(serial.tools.list_ports.comports())
-for port in port_list:
-            Com_Dict["%s" % port[1]] = "%s" % port[1]
-            if(restemser in port[1]):
-                print(port[0])
+class Ser(object):
+    def __init__(self,name):
+        self.name=name
+class name(Ser):
+    def __init__(self,name,bundrate):
+        self.name = name
+        self.bundrate = 9600
 
-port
-port[1]
-port[0]
-port[2]
+
+Switchname = "CH340"
+Switchbundrate = 9600
+def getsername():
+    Com_Dict = {}
+    port_list = list(serial.tools.list_ports.comports())
+    for port in port_list:
+                Com_Dict["%s" % port[1]] = "%s" % port[1]
+                if(Switchname in port[1]):
+                    return(port[0])
+
+
+switchser = serial.Serial(getsername(),Switchbundrate)
+switchser.open
