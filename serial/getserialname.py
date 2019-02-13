0f60a5cb15972c198a56075e1513527affecf4d3
wangyaominde <wangyaominde@qq.com>
2019-02-13 09:28:27 +0800

    changed

--- serial/getserialname.py
+++ serial/getserialname.py
@@ -6,26 +6,38 @@ import serial.tools.list_ports
 import serial
 import string
 import binascii
+import time
 
 class Ser(object):
-    def __init__(self,name):
-        self.name=name
-class name(Ser):
     def __init__(self,name,bundrate):
         self.name = name
-        self.bundrate = 9600
+        self.bundrate = bundrate
 
 
-Switchname = "CH340"
-Switchbundrate = 9600
+class switch(Ser):
+    name = "CH340"
+    bundrate=9600
+
+
+
+
 def getsername():
     Com_Dict = {}
     port_list = list(serial.tools.list_ports.comports())
     for port in port_list:
                 Com_Dict["%s" % port[1]] = "%s" % port[1]
-                if(Switchname in port[1]):
+                if(switch.name in port[1]):
                     return(port[0])
 
 
-switchser = serial.Serial(getsername(),Switchbundrate)
+switchser = serial.Serial(getsername(),switch.bundrate)
+for i in range(10):
+    switchser.write(bytes.fromhex('a0 01 01 a2'))     #接通A
+    switchser.write(bytes.fromhex('a0 02 01 a3'))     #接通B
+    switchser.write(bytes.fromhex('a0 01 00 a1'))     #断开A
+    switchser.write(bytes.fromhex('a0 02 00 a2'))     #断开B
+
+
+
+
 switchser.open
--- serial/serial.py
+++ serial/serial.py
@@ -3,13 +3,13 @@
 # @Last Modified by:   wangyaominde
 # @Last Modified time: 2019-01-03 11:11:36
 # 发送和接收16进制的
-#%%
+
+
 import serial
 import string
 import binascii
 ser = serial.Serial('COM8', 9600)
 ser.open()
-#%%
 '''
 #接收
 n = ser.inwaiting()
@@ -18,12 +18,8 @@ if n:
     print(data)
 #发送
 '''
-#%%
 ser.write(bytes.fromhex('a0 01 01 a2'))     #接通A
-#%%
 ser.write(bytes.fromhex('a0 02 01 a3'))     #接通B
-#%%
 ser.write(bytes.fromhex('a0 01 00 a1'))     #断开A
-#%%
 ser.write(bytes.fromhex('a0 02 00 a2'))     #断开B
 #ser.close()
