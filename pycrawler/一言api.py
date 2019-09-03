9881afd1ace9586cfe91c1ce91b95ec0d28e66b2
wangyaominde <wangyaominde@qq.com>
2019-09-03 10:56:49 +0800

    can show from

--- pycrawler/一言api.py
+++ pycrawler/一言api.py
@@ -1,12 +1,16 @@
-# @Author: wangyaominde  
-# @Date: 2019-01-18 16:13:45  
-# @Last Modified by:   wangyaominde  
+# @Author: wangyaominde
+# @Date: 2019-01-18 16:13:45
+# @Last Modified by:   wangyaominde
 # @Last Modified time: 2019-01-18 16:13:45
 
+import json
 import requests
 import re
 #关闭https证书验证警告
 requests.packages.urllib3.disable_warnings()
-url = "https://v1.hitokoto.cn/?c=d&encode=text"
-r = requests.get(url, verify=False)
-print(r.text)
+#url = "https://v1.hitokoto.cn/?c=d&encode=text"
+#r = requests.get(url, verify=False)
+#print(r.text)
+r = requests.get(url="https://v1.hitokoto.cn/")
+a=r.json()
+print(a['hitokoto']+"---"+a['from'])
