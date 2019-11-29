3d9322fd116d13de094804f0bcf599bdede653a6
wangyaominde <wangyaominde@qq.com>
2019-11-29 15:22:46 +0800

    add tcp

---
+++ tcp/client_tcp.py
@@ -0,0 +1,25 @@
+# TCPclient.py
+
+import socket
+
+target_host = "127.0.0.1" #服务器端地址
+target_port = 51112  #必须与服务器的端口号一致
+
+while True:
+
+    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    client.connect((target_host,target_port))   
+
+    data = input(">")
+
+    if not data:
+        break
+
+    client.send(data.encode())
+
+    response = client.recv(1024)
+
+    print(response)
+
+client.close()
