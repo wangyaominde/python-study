2e6d5925267fa3405c80cf8d04a8a703ea352dc0
wangyaominde <wangyaominde@qq.com>
2020-03-06 10:55:04 +0800

    add flask file

--- flask/flask-admintest.py
+++ flask/flask-admintest.py
@@ -1,9 +1,19 @@
-from flask import Flask
-
-from flask_admin import Admin, BaseView, expose
-
+from flask import Flask,render_template,url_for
 app = Flask(__name__)
 
-admin = Admin(app[,name=u'后台管理系统'])
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-app.run()
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
