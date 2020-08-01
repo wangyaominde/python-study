7416d6d80decd79ba582c932e25141de3bc2d723
wangyaominde <wangyaominde@live.cn>
2020-08-02 01:04:53 +0800

    尝试第二种方法

--- leetcode_Python-master/4.arrays.py
+++ leetcode_Python-master/4.arrays.py
@@ -1,4 +1,8 @@
 #----coding------#
+
+
+'''
+#Solution1
 import numpy as np
 
 class Solution:
@@ -9,3 +13,22 @@ a = Solution()
 nums1 = [1,2]
 nums2 = [3,4]
 print(a.findMedianSortedArrays(nums1,nums2))
+'''
+#Solution2
+
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2) -> float:
+        nums3=[]
+        for x in nums1:
+            for y in nums2:
+                if(x<y):
+                    nums3.append(x)
+                else:
+                    nums3.append(y)
+        return nums3
+
+
+s = Solution()
+nums1 = [1,5]
+nums2 = [3,4]
+print(s.findMedianSortedArrays(nums1,nums2))
